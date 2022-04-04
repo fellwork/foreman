0d9fbfea983c4d478476107c4727919b7612005b
Shane McGuirt <10144278+srmcguirt@users.noreply.github.com>
2022-04-04 10:45:31 -0400

    perf(monorepo): :art: added eslint-config

---
+++ conventions/eslint-config/.prettierrc.js
@@ -0,0 +1,1 @@
+module.exports = '@fellwork/prettier-config-foreman';
---
+++ conventions/eslint-config/config.cjs
@@ -0,0 +1,4 @@
+/**
+ * https://github.com/adaliszk/node-toolbox/blob/8427d113045298b6c52b4c7ab16dd8dbfe0ca48d/packages/eslint-config-typescript/eslint-config.cjs
+ */
+const currentParser = require('@typescript-eslint/parser');
